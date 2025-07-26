c64b19542f0792d6cbf459f252ef9ee035b092d7
Joan Sanchis <jsalvadorsb@gmail.com>
2025-07-26 10:02:45 +0200

    searchbar and grid done, need buttons favorite and filters

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,35 @@
+import "./home.css";
 import { Card } from "../../components/card/card";
+import pokeball_header from "../../img/pokeball_header.svg";
 
 export const Home = () => {
   return (
     <div>
-      <Card />
+      <header className="header">
+        <img src={pokeball_header} alt="" />
+      </header>
+      <div className="controls-container">
+        <div className="searchbar-wrapper">
+          <input
+            className="searchbar"
+            id="search-input"
+            type="search"
+            placeholder="Search by name..."
+          />
+        </div>
+        {/* <div className="filter-buttons-wrapper">
+          <button>Filtros</button>
+          <button>Favoritos</button>
+        </div> */}
+      </div>
+      <div className="grid-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
