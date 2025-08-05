2e4265e686dd65bc78d26a2141846dde84b66abc
Joan Sanchis <jsalvadorsb@gmail.com>
2025-08-05 13:07:13 +0200

    react router installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/home";
 // import { PokemonInfo } from "./pages/pokemonInfo/pokemonInfo";
 
 export const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/pokemon/:id" element={<PokemonInfo />} /> */}
+    </Routes>
   );
 };
